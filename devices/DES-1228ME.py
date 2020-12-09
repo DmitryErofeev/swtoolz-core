c54c6c4d1559b9bcab82e1da0dcbe1f84579095c
erofeev <erofeev@smartintel.ru>
2020-12-09 10:50:14 +0300

    1228

--- devices/DES-1228ME.py
+++ devices/DES-1228ME.py
@@ -123,10 +123,9 @@ walk_PortIndex = {
 
 walk_syslog = {
     'syslog_state': '1.3.6.1.4.1.171.12.12.1.0', # 3 - enabled
-    'SysLogServerIPAddress': '.1.3.6.1.4.1.171.12.12.2.1.2',
-    'SysLogServerSeverity': '.1.3.6.1.4.1.171.12.12.2.1.4',
-    'SysLogServerUDPPort': '.1.3.6.1.4.1.171.12.12.2.1.5',
-
+    'SysLogServerIPAddress': '1.3.6.1.4.1.171.12.12.2.1.2',
+    'SysLogServerSeverity': '1.3.6.1.4.1.171.12.12.2.1.4',
+    'SysLogServerUDPPort': '1.3.6.1.4.1.171.12.12.2.1.5',
 }
 
 get_PortIndex = {

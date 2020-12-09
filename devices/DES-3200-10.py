47a069473854e348eb49e6e0d53dbb8338c1ae15
erofeev <erofeev@smartintel.ru>
2020-12-09 13:27:38 +0300

    3200-10

--- devices/DES-3200-10.py
+++ devices/DES-3200-10.py
@@ -136,10 +136,10 @@ walk_PortIndex = {
     }
 
 walk_syslog = {
-    'syslog_state': '1.3.6.1.4.1.171.12.12.1.0', # 3 - enabled
-    'SysLogServerIPAddress': '.1.3.6.1.4.1.171.12.12.2.1.2',
-    'SysLogServerSeverity': '.1.3.6.1.4.1.171.12.12.2.1.4',
-    'SysLogServerUDPPort': '.1.3.6.1.4.1.171.12.12.2.1.5',
+    'syslog_state': '1.3.6.1.4.1.171.12.12.2.1.6', # 3 - enabled
+    'hex_ip:SysLogServerIPAddress': '1.3.6.1.4.1.171.12.12.2.1.9',
+    'SysLogServerSeverity': '1.3.6.1.4.1.171.12.12.2.1.4',
+    'SysLogServerUDPPort': '1.3.6.1.4.1.171.12.12.2.1.5',
 
 }
 
